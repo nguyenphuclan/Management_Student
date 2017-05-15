fc1d52817fb4f6e20a961c6268ec974adf564b1f
Lan Nguyen <lannguyen@Lans-Mac.local>
2017-05-15 22:15:28 +0700

    Initial Commit

---
+++ ManagementStudent/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ManagementStudent
+//
+//  Created by Lan Nguyen on 5/15/17.
+//  Copyright © 2017 Lan Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
